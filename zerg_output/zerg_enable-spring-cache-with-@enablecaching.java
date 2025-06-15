8bd80f2b7764bdd94bb29dd4a5b64242478239e3
Prabhu Ramanathan Babu <c-prabhu@lily.ai>
2025-06-15 17:27:45 +0530

    Enable Spring Cache with @EnableCaching implementation

---
+++ zerg_output/zerg_enable-spring-cache-with-@enablecaching.java
@@ -0,0 +1,16 @@
+```java
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class AppConfig {
+
+    @Bean
+    public ConcurrentMapCacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("productTypeCache");
+    }
+}
+```
