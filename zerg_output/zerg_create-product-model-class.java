979c2c124dc7386f555440753fcf16495c500bd5
Prabhu Ramanathan Babu <c-prabhu@lily.ai>
2025-06-15 17:27:05 +0530

    Create Product model class implementation

---
+++ zerg_output/zerg_create-product-model-class.java
@@ -0,0 +1,37 @@
+```java
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_description")
+    private String productDescription;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "stock_quantity")
+    private Integer stockQuantity;
+}
+```
