582d0b8ceb7ef410467f528655b2be10b97c8df1
Prabhu Ramanathan Babu <c-prabhu@lily.ai>
2025-06-17 17:08:01 +0530

    Enable Spring Cache with @EnableCaching implementation

---
+++ src/main/java/com/lilyai/producttypedetection/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.lilyai.producttypedetection.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class AppConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("productTypeCache");
+    }
+}
